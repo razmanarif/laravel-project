1efd048fd760e1d308c15c70011debac4f585919
razmanarif <razmanarif@gmail.com>
2023-08-14 23:14:50 +0800

    Initial laravel setup

---
+++ resources/js/Home.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <strong>I am a vue app</strong>
+</template>
---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import Home from './Home.vue'
+
+import {createApp} from 'vue';
+
+createApp(Home).mount('#app');
